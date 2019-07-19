2fe3bca8afefabe66bf7a07d2c1b6b79f9ba80a5
Jim DeLois <jimdelois@users.noreply.github.com>
2019-07-19 11:53:54 -0400

    Lesson 13: Understanding State by Adding Fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,19 +1,29 @@
 import React  from 'react';
-
-import Header from './Header';
 import Inventory from './Inventory';
-import Order from './Order';
-
 
 class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+        // Take a copy of the existing state (no need for deep clone)
+        const fishes = { ...this.state.fishes };
+        // Adjust and replace.
+        fishes[`fish${Date.now()}`] = fish;
+        this.setState({ fishes });
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh-ish Seafood" />
-                </div>
-                <Order />
-                <Inventory />
+                {/*<div className="menu">*/}
+                {/*    <Header tagline="Fresh-ish Seafood" />*/}
+                {/*</div>*/}
+                {/*<Order />*/}
+                <Inventory addFish={this.addFish}/>
             </div>
         );
     }
