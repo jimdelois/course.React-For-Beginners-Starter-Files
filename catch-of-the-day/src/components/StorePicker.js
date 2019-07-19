148e22f10ade1a0a4eeb4d3f8a95706f24571e86
Jim DeLois <jimdelois@users.noreply.github.com>
2019-07-18 20:11:09 -0400

    Lesson 4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,12 +4,11 @@ import React , { Fragment } from 'react';
 class StorePicker extends React.Component {
     render() {
         return (
-            <Fragment>
-                <h1>Hello, from the Picker!</h1>
-                <p>The "React.Fragment" component (new in React 16) allows for the creation of adjacent elements, which
-                    is otherwise prohibited in previous versions. Further, it nicely renders nothing in its place,
-                    so there are no longer any silly top-level DIVs unnecessarily.</p>
-            </Fragment>
+            <form className="store-selector">
+                <h2>Please Enter a Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>
         );
     }
 }
