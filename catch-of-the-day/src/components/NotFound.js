1f1153d74c7ae7434d7a40a3353817a1cf179156
Jim DeLois <jimdelois@users.noreply.github.com>
2019-07-19 10:48:00 -0400

    Lesson 9: Basic React BrowserRouting

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+    <div className="notFound">
+        <h2>Not Found!!! (WAT)</h2>
+    </div>
+);
+
+export default NotFound;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components/App';
+import Router from './components/Router';
 import './css/style.css';
 
-render(<App />, document.querySelector('div#main'));
+render(<Router />, document.querySelector('div#main'));
