0ab18793f69a09e9f216924483a7d29e42de86a6
Jim DeLois <jimdelois@users.noreply.github.com>
2019-07-19 11:05:58 -0400

    Lesson 11: Binding This in Custom Method

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef();
+
+    goToStore = (e) => {
+        // Stop the form from Submitting by usual means
+        e.preventDefault();
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input
+                    type="text"
+                    required
+                    ref={this.myInput}
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                />
                 <button type="submit">Visit Store</button>
             </form>
         );
