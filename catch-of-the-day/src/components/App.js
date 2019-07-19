acd48894abf9711538d974bd85f2ff89baf5b01f
Jim DeLois <jimdelois@users.noreply.github.com>
2019-07-19 01:34:06 -0400

    Lesson 7: Basic Props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Fresh-ish Seafood" />
                 </div>
                 <Order />
                 <Inventory />
