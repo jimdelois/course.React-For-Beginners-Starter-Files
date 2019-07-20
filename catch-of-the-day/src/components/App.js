d11e7d86afca520b66f887750a3462b7b9edda5b
Jim DeLois <jimdelois@users.noreply.github.com>
2019-07-20 10:49:23 -0400

    Lesson 15: Creating a List with State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,6 +1,9 @@
 import React  from 'react';
+import Header from './Header';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+
 
 class App extends React.Component {
 
@@ -24,9 +27,14 @@ class App extends React.Component {
     render() {
         return (
             <div className="catch-of-the-day">
-                {/*<div className="menu">*/}
-                {/*    <Header tagline="Fresh-ish Seafood" />*/}
-                {/*</div>*/}
+                <div className="menu">
+                    <Header tagline="Fresh-ish Seafood" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map((key) => {
+                            return <Fish key={key} details={this.state.fishes[key]}/>
+                        })}
+                    </ul>
+                </div>
                 {/*<Order />*/}
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
